981fbe3bb3c13b08047cb59a276469c2c355dc86
Irwan Nawawi <muhammadirwannawawi@gmail.com>
2024-01-03 12:05:03 +0700

    membuat tabel jenisBarang

---
+++ TubesFinalKel/projek/app/Models/jenisBarang.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class jenisBarang extends Model
+{
+    use HasFactory;
+}
